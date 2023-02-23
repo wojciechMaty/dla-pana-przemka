f0b245cc75f6240f992da42cebcee4ad15515a4e
wojciechMaty <99251060+wojciechMaty@users.noreply.github.com>
2023-02-23 08:28:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function oblicz(){
+    var a1=parseFloat(document.getElementById("a1").value);
+    var an=parseFloat(document.getElementById("an").value);
+    var n=parseFloat(document.getElementById("n").value);
+    var wynik=(an-a1)/(n-1);
+}
